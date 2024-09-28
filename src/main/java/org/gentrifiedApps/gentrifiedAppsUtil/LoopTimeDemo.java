54a49209b6c7eb23253829f51609d654003d1832
Graden <101433010+GramGra07@users.noreply.github.com>
2024-09-28 15:31:58 -0600

    updates to loop time controller

---
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/LoopTimeDemo.java
@@ -0,0 +1,22 @@
+package org.gentrifiedApps.gentrifiedAppsUtil;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+//@Disabled
+
+class LoopTimeDemo extends LinearOpMode {
+    public void runOpMode() {
+        ElapsedTime elapsedTime = new ElapsedTime();
+        LoopTimeController loopTimeController = new LoopTimeController(elapsedTime,null);
+        waitForStart();
+        while (opModeIsActive()) {
+            loopTimeController.update();
+
+            loopTimeController.telemetry(telemetry);
+            telemetry.update();
+        }
+    }
+}
