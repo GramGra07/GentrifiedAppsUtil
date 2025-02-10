02b1f66c74fdc6118206d28908461cf0dada45a3
Graden <101433010+GramGra07@users.noreply.github.com>
2025-02-10 11:05:48 -0700

    added init movement

---
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/initMovement/InitOpMode.java
@@ -0,0 +1,28 @@
+package org.gentrifiedApps.gentrifiedAppsUtil.initMovement;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+@Disabled
+public class InitOpMode extends LinearOpMode {
+    InitMovementController initMovementController;
+
+    @Override
+    public void runOpMode() {
+        initMovementController = new InitMovementController(gamepad1, gamepad2);
+        waitForStart();
+        while (opModeIsActive()) {
+            initMovementController.checkHasMovedOnInit();
+            if (initMovementController.hasMovedOnInit()) {
+                telemetry.addData("Status", "Moved");
+                telemetry.update();
+                break;
+            } else {
+                telemetry.addData("Status", "Not Moved");
+                telemetry.update();
+            }
+        }
+    }
+}
