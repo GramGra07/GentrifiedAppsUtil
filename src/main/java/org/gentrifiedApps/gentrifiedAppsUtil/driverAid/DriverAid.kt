0ba79c39d180b3f6cbe6618305bd509ef7288a37
Graden <101433010+GramGra07@users.noreply.github.com>
2025-02-09 18:07:13 -0700

    driver aid added

---
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/driverAid/DriverAid.kt
@@ -0,0 +1,75 @@
+package org.gentrifiedApps.gentrifiedAppsUtil.driverAid
+
+import org.jetbrains.annotations.TestOnly
+
+/**
+ * A class to help with driver aid actions, automatically and continuously running functions
+ * based on the state of the robot and what is sent.
+ * @param T The enum class to use
+ * @param enumClass The enum class to use
+ * @property daState The current state of the driver aid
+ * @see DAFunc
+ */
+class DriverAid<T:Enum<T>>(enumClass: Class<T>) {
+    var daState : Enum<T>? = null
+    private var daFunc : DAFunc<T>? = null
+
+    @TestOnly
+    fun getDAFunc() = daFunc
+
+    init {
+        require(enumClass.enumConstants?.any { it.name == "IDLE" } == true) {
+            "Enum class must have an IDLE constant"
+        }
+    }
+    fun update() {
+        daFunc?.runALot()
+    }
+
+    /**
+     * A function to run functions based on the state of the robot.
+     * @param driverAid The driver aid class to use
+     * @param state The state to run the functions in
+     * @param funcs The functions to run only once
+     * @param runConstant The functions to run constantly
+     * @param isEnded A lambda to check if the functions are done
+     * @param resetOnInit A lambda to run when the functions are initialized
+     * @constructor Creates a new driver aid function
+     */
+    class DAFunc<T:Enum<T>>(
+        private val driverAid: DriverAid<T>,
+        val state:T,
+        private val funcs: Runnable,
+        private val runConstant: Runnable?,
+        private val isEnded: java.util.function.BooleanSupplier,
+        private val resetOnInit : Runnable?
+    ) {
+        fun runInit() {
+            driverAid.daState = state
+            funcs.run()
+            driverAid.daFunc = this
+            resetOnInit?.run()
+        }
+
+        fun runALot() {
+            runConstant?.run()
+        }
+        fun isEnded(): Boolean {
+            return isEnded.asBoolean
+        }
+    }
+
+    //TODO show action implementation
+//    class DAActions(
+//        private val funcs: List<Runnable>,
+//    ) : Action {
+//        override fun run(packet: TelemetryPacket): Boolean {
+//            funcs.forEach(Runnable::run)
+//            return false
+//        }
+//    }
+//
+//    fun daAction(funcs: List<Runnable>): Action {
+//        return DAActions(funcs)
+//    }
+}
---
+++ src/main/java/test/kotlin/DATest.kt
@@ -0,0 +1,128 @@
+package test.kotlin;
+
+import org.gentrifiedApps.gentrifiedAppsUtil.driverAid.DriverAid
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.util.function.BooleanSupplier
+
+public class DATest {
+    enum class TestState {
+        IDLE, ACTIVE
+    }
+    @Test
+    fun testDriverAidInitialization() {
+        val driverAid = DriverAid(TestState::class.java)
+        assertNotNull(driverAid)
+    }
+
+    @Test
+    fun testDAFuncRunInit() {
+        val driverAid = DriverAid(TestState::class.java)
+        val funcs = Runnable { println("Running funcs") }
+        val runConstant = Runnable { println("Running constant") }
+        val isEnded = BooleanSupplier { false }
+        val resetOnInit = Runnable { println("Reset on init") }
+
+        val daFunc = DriverAid.DAFunc(
+            driverAid,
+                state = TestState.IDLE,
+                funcs = funcs,
+                runConstant = runConstant,
+                isEnded = isEnded,
+                resetOnInit = resetOnInit
+        )
+        daFunc.runInit()
+
+        driverAid.update()
+        assertEquals(TestState.IDLE, driverAid.daState)
+        assertNotNull(driverAid.getDAFunc())
+    }
+
+    @Test
+    fun testDAFuncRunALot() {
+        val driverAid = DriverAid(TestState::class.java)
+        val funcs = Runnable { println("Running funcs") }
+        val runConstant = Runnable { println("Running constant") }
+        val isEnded = BooleanSupplier { false }
+        val resetOnInit = Runnable { println("Reset on init") }
+
+        val daFunc = DriverAid.DAFunc(
+            driverAid,
+                state = TestState.IDLE,
+                funcs = funcs,
+                runConstant = runConstant,
+                isEnded = isEnded,
+                resetOnInit = resetOnInit
+        )
+        daFunc.runInit()
+
+        driverAid.update()
+        assertFalse(daFunc.isEnded())
+        assertTrue(true) // Just to ensure the test runs without exceptions
+    }
+    // test multiple functions
+
+    @Test
+    fun testDriverAidUpdate() {
+        val driverAid = DriverAid(TestState::class.java)
+        val funcs = Runnable { println("Running funcs") }
+        val runConstant = Runnable { println("Running constant") }
+        val isEnded = BooleanSupplier { false }
+        val resetOnInit = Runnable { println("Reset on init") }
+        val daFunc = DriverAid.DAFunc(
+                driverAid,
+                state = TestState.IDLE,
+                funcs = funcs,
+                runConstant = runConstant,
+                isEnded = isEnded,
+                resetOnInit = resetOnInit
+        )
+        daFunc.runInit()
+        driverAid.update()
+        assertFalse(daFunc.isEnded())
+        assertTrue(true) // Just to ensure the test runs without exceptions
+        // new test
+
+        val funcs2 = Runnable { println("Running funcs 2") }
+        val runConstant2 = Runnable { println("Running constant 2") }
+        val isEnded2 = BooleanSupplier { false }
+        val resetOnInit2 = Runnable { println("Reset on init 2") }
+        val daFunc2 = DriverAid.DAFunc(
+                driverAid,
+                state = TestState.IDLE,
+                funcs = funcs2,
+                runConstant = runConstant2,
+                isEnded = isEnded2,
+                resetOnInit = resetOnInit2
+        )
+        daFunc2.runInit()
+        driverAid.update()
+        assertFalse(daFunc2.isEnded())
+        assertTrue(true) // Just to ensure the test runs without exceptions
+    }
+    @Test
+    fun testAllAreRun(){
+        var storage = BooleanArray(3, { false })
+        val driverAid = DriverAid(TestState::class.java)
+        val funcs = Runnable { storage[0] = true }
+        val runConstant = Runnable { storage[1] = true }
+        val isEnded = BooleanSupplier { storage.contentEquals(BooleanArray(3) { true }) }
+        val resetOnInit = Runnable { storage[2] = true }
+        val daFunc = DriverAid.DAFunc(
+                driverAid,
+                state = TestState.IDLE,
+                funcs = funcs,
+                runConstant = runConstant,
+                isEnded = isEnded,
+                resetOnInit = resetOnInit
+        )
+        daFunc.runInit()
+        driverAid.update()
+        println(storage.contentToString())
+        assertTrue(daFunc.isEnded())
+        assertTrue(true) // Just to ensure the test runs without exceptions
+    }
+}
