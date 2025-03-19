43ef0aa36ceca763e87811ef1efe8aaecdf20ef4
Graden <101433010+GramGra07@users.noreply.github.com>
2025-03-19 15:44:25 -0600

    subsystem update

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/Subsystem.kt
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/Subsystem.kt
@@ -6,11 +6,15 @@ import org.firstinspires.ftc.robotcore.external.Telemetry
 abstract class Subsystem(private val hwMap: HardwareMap, auto: Boolean = false) {
     constructor(hwMap: HardwareMap) : this(hwMap, false)
 
+    abstract fun init()
     abstract fun update()
     abstract fun telemetry(telemetry: Telemetry)
 }
 
 class ArmSub(hwMap: HardwareMap) : Subsystem(hwMap) {
+    override fun init() {
+
+    }
     override fun update() {
 
     }
