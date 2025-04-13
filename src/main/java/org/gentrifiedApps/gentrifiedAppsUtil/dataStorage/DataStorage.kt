cdf29fb218d4bccac150a1d83ba7b2da4e4d57e3
Graden <101433010+GramGra07@users.noreply.github.com>
2025-04-12 22:56:06 -0600

    added data storage

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/dataStorage/DataStorage.kt
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/dataStorage/DataStorage.kt
@@ -4,22 +4,88 @@ import org.gentrifiedApps.gentrifiedAppsUtil.classes.generics.Alliance
 import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.Target2D
 
 class DataStorage {
-    companion object Storage{
+    companion object Storage {
         private var currentPose = Target2D.blank()
         private var alliance: Alliance = Alliance.RED
 
 
-        @JvmStatic fun setAlliance(alliance: Alliance) {
+        /**
+         * Sets the alliance
+         * @param alliance The alliance to set
+         */
+        @JvmStatic
+        fun setAlliance(alliance: Alliance) {
             this.alliance = alliance
         }
-        @JvmStatic fun getAlliance(): Alliance {
+
+        /**
+         * Gets the alliance
+         * @return The alliance
+         */
+        @JvmStatic
+        fun getAlliance(): Alliance {
             return alliance
         }
-        @JvmStatic fun setPose(pose: Target2D) {
+
+        /**
+         * Sets the pose
+         * @param pose The pose to set
+         */
+        @JvmStatic
+        fun setPose(pose: Target2D) {
             currentPose = pose
         }
-        @JvmStatic fun getPose(): Target2D {
+
+        /**
+         * Gets the pose
+         * @return The pose
+         */
+        @JvmStatic
+        fun getPose(): Target2D {
             return currentPose
+        }
+
+        internal fun getData(): Pair<Alliance, Target2D> {
+            return Pair(alliance, currentPose)
+        }
+
+        /**
+         * Sets the data
+         * @param alliance The alliance to set
+         * @param pose The pose to set
+         */
+        @JvmStatic
+        fun setData(alliance: Alliance, pose: Target2D) {
+            this.alliance = alliance
+            currentPose = pose
+        }
+
+        internal val dataStore = DataStore()
+
+        /**
+         * Initializes the data store to file
+         */
+        @JvmStatic
+        fun initDataStore() {
+            dataStore.init()
+        }
+
+        /**
+         * Writes the data store to file direct from the DataStorage
+         */
+        @JvmStatic
+        fun writeDataStore() {
+            dataStore.writeToFile()
+        }
+
+        /**
+         * Reads the data store from file and sets the data in the DataStorage
+         */
+        @JvmStatic
+        fun readDataStore() {
+            val data = dataStore.readFromFile()
+            this.setPose(data.second)
+            this.setAlliance(data.first)
         }
     }
 }
---
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/dataStorage/DataStore.kt
@@ -0,0 +1,85 @@
+package org.gentrifiedApps.gentrifiedAppsUtil.dataStorage
+
+import android.os.Environment
+import org.gentrifiedApps.gentrifiedAppsUtil.classes.Scribe
+import org.gentrifiedApps.gentrifiedAppsUtil.classes.generics.Alliance
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.Angle
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.AngleUnit
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.pointClasses.Target2D
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
+internal class DataStore {
+    private lateinit var file: File
+    private val tag = "store"
+    internal fun init() {
+        createFiles()
+        writeToFile()
+    }
+
+    internal fun createFiles() {
+        val filePath =
+            "${Environment.getExternalStorageDirectory().absolutePath}/FIRST/dataStore/$tag.txt"
+        try {
+            val file = File(filePath)
+            file.parentFile.mkdirs()
+            if (file.exists()) {
+                file.delete()
+            }
+            file.createNewFile()
+            this.file = file
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    internal fun writeToFile() {
+        val dataStore = DataStorage.getData()
+        val alliance = dataStore.first
+        val pose = dataStore.second.toPose2D() // everything in rad
+        val data = "$alliance,$pose.x,$pose.y,$pose.heading"
+        if (file.exists()) {
+            try {
+                val fileWriter = FileWriter(file, false)
+                fileWriter.write(data.toString())
+                fileWriter.close()
+                Scribe.instance.setSet("DataStore")
+                    .logDebug("Data written to file: $data")
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Scribe.instance.setSet("DataStore")
+                    .logError("Failed to write to file: ${e.message}")
+            }
+            Scribe.instance.setSet("DataStore")
+                .logDebug("Data written to file: $data")
+        } else {
+            Scribe.instance.setSet("DataStore").logWarning("File does not exist")
+            throw IOException("File does not exist")
+        }
+    }
+
+    internal fun readFromFile(): Pair<Alliance, Target2D> {
+        val lines = file.readLines()
+        for (line in lines) {
+            if (line.isEmpty()) {
+                lines.drop(lines.indexOf(line))
+            }
+            if (line.split(',').size != 4) {
+                Scribe.instance.setSet("DataStore")
+                    .logError("Line: ${lines.indexOf(line)} is not in the correct format, is instead: $line")
+            }
+        }
+        val line = lines[0].split(',')
+        return Pair(
+            Alliance.fromString(line[0]),
+            Target2D(
+                line[1].toDouble(),
+                line[2].toDouble(),
+                Angle(
+                    line[3].toDouble(), AngleUnit.RADIANS
+                )
+            )
+        )
+    }
+}
