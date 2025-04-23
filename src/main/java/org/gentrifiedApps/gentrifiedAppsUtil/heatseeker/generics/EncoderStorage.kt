e61fbed1825cac76b80e1e71e1cebd33cc0d458a
Graden <101433010+GramGra07@users.noreply.github.com>
2025-04-22 19:25:38 -0600

    branched hs

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/heatseeker/generics/EncoderStorage.kt
+++
@@ -1,7 +0,0 @@
-package org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics
-
-data class EncoderStorage(
-    val par1Encoder: Encoder,
-    val par2Encoder: Encoder?,
-    val perpEncoder: Encoder
-)
