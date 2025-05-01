7b24a1a88db0d60539031788bc6974bc91892207
Graden <101433010+GramGra07@users.noreply.github.com>
2025-05-01 14:39:07 -0600

    added to binary array

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/BinaryArray.kt
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/BinaryArray.kt
@@ -2,6 +2,7 @@ package org.gentrifiedApps.gentrifiedAppsUtil.classes
 
 import org.gentrifiedApps.gentrifiedAppsUtil.classes.generics.Alliance
 import org.gentrifiedApps.gentrifiedAppsUtil.classes.vision.Color
+import kotlin.math.pow
 
 /**
  * A class to represent a binary array of 1 and 0
@@ -10,6 +11,12 @@ import org.gentrifiedApps.gentrifiedAppsUtil.classes.vision.Color
  * @see Color
  */
 class BinaryArray(size: Int) {
+    constructor(vararg elements: Double) : this(elements.size) {
+        for (i in elements.indices) {
+            array[i] = elements[i]
+        }
+    }
+
     private val array: DoubleArray = DoubleArray(size)
 
     // Add methods to manipulate the array as needed
@@ -23,6 +30,10 @@ class BinaryArray(size: Int) {
 
     fun size(): Int {
         return array.size
+    }
+
+    override fun toString(): String {
+        return array.joinToString(", ")
     }
 
     /**
@@ -63,5 +74,65 @@ class BinaryArray(size: Int) {
             1.0 -> Alliance.BLUE
             else -> Alliance.RED
         }
+    }
+
+    fun readBase(base: Int): BinaryArray {
+        var value = 0
+        for (i in 0 until size()) {
+            value += (this[i] * base.toDouble().pow(i.toDouble())).toInt()
+        }
+
+        return BinaryArray(*value.toString(base).map { it.toString().toDouble() }.toDoubleArray())
+    }
+
+    fun readBase10(): BinaryArray {
+        return readBase(10)
+    }
+
+    fun readBase2(): BinaryArray {
+        return readBase(2)
+    }
+
+    fun readOctal(): BinaryArray {
+        return readBase(8)
+    }
+
+    fun readHexadecimal(): BinaryArray {
+        return readBase(16)
+    }
+
+    fun toBase(base: Int): BinaryArray {
+        // Always read the binary array as base 2 first
+        var value = 0
+        for (i in 0 until size()) {
+            value += (this[i] * 2.0.pow(size() - 1 - i)).toInt()
+        }
+
+        // Convert that base-10 value to the target base
+        val result = mutableListOf<Double>()
+        if (value == 0) result.add(0.0)
+        while (value > 0) {
+            result.add((value % base).toDouble())
+            value /= base
+        }
+
+        return BinaryArray(*result.reversed().toDoubleArray())
+    }
+
+
+    fun toBase10(): BinaryArray {
+        return toBase(10)
+    }
+
+    fun toBase2(): BinaryArray {
+        return toBase(2)
+    }
+
+    fun toOctal(): BinaryArray {
+        return toBase(8)
+    }
+
+    fun toHexadecimal(): BinaryArray {
+        return toBase(16)
     }
 }
