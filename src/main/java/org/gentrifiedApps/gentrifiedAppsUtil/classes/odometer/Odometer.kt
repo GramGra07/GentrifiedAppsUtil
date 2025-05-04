73fe8e73d883d8e8ea51a8c907806eb94d0081f0
Graden <101433010+GramGra07@users.noreply.github.com>
2025-05-04 14:25:24 -0600

    added odometer

---
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/odometer/Odometer.kt
@@ -0,0 +1,148 @@
+package org.gentrifiedApps.gentrifiedAppsUtil.classes.odometer
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.gentrifiedApps.gentrifiedAppsUtil.classes.generics.pointClasses.Point
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.Driver
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.EncoderSpecs
+import kotlin.math.sqrt
+
+abstract class Odometer(open val opMode: OpMode?) {
+    abstract val odometerFileManager: OdometerFileManager
+    open fun update() {}
+    open fun update(point: Point) {}
+    abstract var ODO: Double
+    open fun addConstraint(encoderSpecs: EncoderSpecs) {}
+    enum class ReturnType {
+        RAW_TICKS,
+        INCHES,
+        METERS,
+        MILES;
+
+        fun getUnit(): String {
+            return when (this) {
+                RAW_TICKS -> "ticks"
+                INCHES -> "inches"
+                METERS -> "meters"
+                MILES -> "miles"
+            }
+        }
+    }
+
+    abstract var returnType: ReturnType
+    open var encoderSpecs: EncoderSpecs? = null
+    abstract fun telemetry(telemetry: Telemetry)
+    abstract fun applyMultiplier(): Double
+    open fun switchMult(ticksPerIn: Double) {}
+    open fun switchMult() {}
+
+    companion object {
+        @JvmStatic
+        fun newOdometer(driver: Driver): Odometer {
+            return DriveOdometer(driver)
+        }
+
+        @JvmStatic
+        fun newOdometer(opMode: OpMode, startPoint: Point): Odometer {
+            return LocalizerOdometer(opMode, startPoint)
+        }
+    }
+}
+
+class DriveOdometer(val driver: Driver) : Odometer(driver.opMode) {
+    override val odometerFileManager: OdometerFileManager = OdometerFileManager()
+    override var ODO: Double = odometerFileManager.readOdometryData()
+    override var encoderSpecs: EncoderSpecs? = odometerFileManager.readConfigData()
+
+    override fun update() {
+        val poses = driver.getPositions()
+        ODO += poses.abs().average()
+        odometerFileManager.writeOdometryData(ODO)
+        encoderSpecs = odometerFileManager.readConfigData()
+    }
+
+    override fun addConstraint(encoderSpecs: EncoderSpecs) {
+        this.encoderSpecs = encoderSpecs
+        odometerFileManager.writeConfigData(encoderSpecs.ticksPerInch.toString())
+    }
+
+    override var returnType: ReturnType = ReturnType.RAW_TICKS
+    override fun applyMultiplier(): Double {
+        if (encoderSpecs == null) {
+            return ODO
+        }
+        return when (returnType) {
+            ReturnType.RAW_TICKS -> ODO
+            ReturnType.INCHES -> ticksToInches(ODO.toInt())
+            ReturnType.METERS -> ticksToInches(ODO.toInt()) * 0.0254
+            ReturnType.MILES -> ticksToInches(ODO.toInt()) / (5280 * 12)
+        }
+    }
+
+    private fun ticksToInches(ticks: Int): Double {
+        return ticks.toDouble() / encoderSpecs!!.ticksPerInch
+    }
+
+    override fun switchMult(ticksPerIn: Double) {
+        if (encoderSpecs == null) {
+            return
+        }
+        val inches = ticksToInches(ODO.toInt())
+        returnType = when {
+            inches > 5280 * 12 -> ReturnType.MILES // 5280 feet in a mile, converted to inches
+            inches > 39.37 -> ReturnType.METERS // 1 meter = 39.37 inches
+            inches > 2 -> ReturnType.INCHES
+            else -> ReturnType.RAW_TICKS
+        }
+    }
+
+    override fun toString(): String {
+        switchMult(encoderSpecs!!.ticksPerInch)
+        return "DriveOdometer - ${applyMultiplier()}"
+    }
+
+    override fun telemetry(telemetry: Telemetry) {
+        telemetry.addLine(this.toString())
+    }
+}
+
+class LocalizerOdometer(override val opMode: OpMode, val startPoint: Point) : Odometer(opMode) {
+    override val odometerFileManager: OdometerFileManager = OdometerFileManager()
+    override var ODO: Double = odometerFileManager.readOdometryData()
+    var lastPoint = startPoint
+    override fun update(point: Point) {
+        // get hypotenuse
+        val x = point.x - lastPoint.x
+        val y = point.y - lastPoint.y
+        val hypotenuse = sqrt(x * x + y * y)
+        ODO += hypotenuse
+        lastPoint = point
+    }
+
+    override var returnType: ReturnType = ReturnType.INCHES
+    override fun applyMultiplier(): Double {
+        return when (returnType) {
+            ReturnType.INCHES -> ODO
+            ReturnType.METERS -> ODO * 0.0254
+            ReturnType.MILES -> ODO * 0.0000157828283
+            else -> ODO
+        }
+    }
+
+    override fun switchMult() {
+        // already in inches
+        returnType = when {
+            ODO > 5280 * 12 -> ReturnType.MILES
+            ODO > 39.37 -> ReturnType.METERS
+            else -> ReturnType.INCHES
+        }
+    }
+
+    override fun toString(): String {
+        return "LocalizerOdometer - ${applyMultiplier()}"
+    }
+
+    override fun telemetry(telemetry: Telemetry) {
+        telemetry.addLine(this.toString())
+    }
+}
---
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/odometer/OdometerFileManager.kt
@@ -0,0 +1,146 @@
+package org.gentrifiedApps.gentrifiedAppsUtil.classes.odometer
+
+import android.os.Environment
+import org.gentrifiedApps.gentrifiedAppsUtil.classes.Scribe
+import org.gentrifiedApps.gentrifiedAppsUtil.heatseeker.generics.EncoderSpecs
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
+class OdometerFileManager {
+    val odometerFileName = "odometer.txt"
+    val odometerConfig = "odometerConfig.txt"
+
+    lateinit var odometerFile: File
+    lateinit var odometerConfigFile: File
+    var odoFileExists = false
+    var odoConfigFileExists = false
+
+    init {
+        getFiles()
+        createIfNotExists()
+    }
+
+    fun getFiles() {
+        val filePath =
+            "${Environment.getExternalStorageDirectory().absolutePath}/FIRST/odometer/$odometerFileName"
+        try {
+            val file = File(filePath)
+            this.odometerFile = file
+            if (file.exists()) {
+                this.odoFileExists = true
+            }
+            Scribe.instance.setSet("Odometer").logDebug("File path: $filePath")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        val configPath =
+            "${Environment.getExternalStorageDirectory().absolutePath}/FIRST/odometer/$odometerConfig"
+        try {
+            val file = File(configPath)
+            this.odometerConfigFile = file
+            if (file.exists()) {
+                this.odoConfigFileExists = true
+            }
+            Scribe.instance.setSet("Odometer").logDebug("File path: $configPath")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun createIfNotExists() {
+        val filePath =
+            "${Environment.getExternalStorageDirectory().absolutePath}/FIRST/odometer/$odometerFileName"
+        try {
+            val file = File(filePath)
+            file.parentFile.mkdirs()
+            if (file.exists()) {
+                file.delete()
+            }
+            file.createNewFile()
+            this.odometerFile = file
+            this.odoFileExists = true
+            writeOdometryData(0.0)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun writeOdometryData(total: Double) {
+        if (odometerFile.exists()) {
+            try {
+                val fileWriter = FileWriter(odometerFile, false) // Overwrite mode
+                fileWriter.write(total.toString())
+                fileWriter.close()
+                Scribe.instance.setSet("Odometer").logDebug("Data written to file: $total")
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Scribe.instance.setSet("Odometer").logError("Failed to write to file: ${e.message}")
+            }
+        } else {
+            Scribe.instance.setSet("Odometer").logWarning("File does not exist")
+            throw IOException("File does not exist")
+        }
+    }
+
+    fun writeConfigData(config: String) {
+        if (odometerConfigFile.exists()) {
+            try {
+                val fileWriter = FileWriter(odometerConfigFile, false) // Overwrite mode
+                fileWriter.write(config)
+                fileWriter.close()
+                Scribe.instance.setSet("Odometer").logDebug("Data written to file: $config")
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Scribe.instance.setSet("Odometer").logError("Failed to write to file: ${e.message}")
+            }
+        } else {
+            Scribe.instance.setSet("Odometer").logWarning("File does not exist")
+            throw IOException("File does not exist")
+        }
+    }
+
+    fun readOdometryData(): Double {
+        if (odometerFile.exists()) {
+            try {
+                val lines = odometerFile.readLines().filter { it.isNotEmpty() }
+                if (lines.isNotEmpty()) {
+                    return lines[0].toDouble()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Scribe.instance.setSet("Odometer")
+                    .logError("Failed to read odometry data: ${e.message}")
+            }
+        } else {
+            Scribe.instance.setSet("Odometer").logWarning("File does not exist")
+            throw IOException("File does not exist")
+        }
+        return 0.0
+    }
+
+    fun readConfigData(): EncoderSpecs? {
+        if (odometerConfigFile.exists()) {
+            try {
+                val lines = odometerConfigFile.readLines()
+                if (lines.size > 1) {
+                    Scribe.instance.setSet("Odometer")
+                        .logDebug("Error reading config file, too many lines")
+                } else if (lines.isEmpty()) {
+                    Scribe.instance.setSet("Odometer")
+                        .logDebug("Error reading config file, empty file")
+                    return EncoderSpecs(0.0)
+                } else {
+                    val line = lines[0]
+                    return EncoderSpecs(line.toDouble())
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        } else {
+            Scribe.instance.setSet("Odometer").logWarning("File does not exist")
+            throw IOException("File does not exist")
+        }
+        return null
+    }
+}
