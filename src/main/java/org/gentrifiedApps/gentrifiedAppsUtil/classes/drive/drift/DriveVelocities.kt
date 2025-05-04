2db6787449bc3b1a7bad18e5cdaa3e8220ed9183
Graden <101433010+GramGra07@users.noreply.github.com>
2025-05-04 14:25:13 -0600

    few more useful functions

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/drive/drift/DriveVelocities.kt
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/classes/drive/drift/DriveVelocities.kt
@@ -8,8 +8,14 @@ data class DriveVelocities(
     var frontRight: Double,
     var backLeft: Double,
     var backRight: Double
-){
-    constructor(fl: Int, fr: Int, bl: Int, br: Int) : this(fl.toDouble(), fr.toDouble(), bl.toDouble(), br.toDouble())
+) {
+    constructor(fl: Int, fr: Int, bl: Int, br: Int) : this(
+        fl.toDouble(),
+        fr.toDouble(),
+        bl.toDouble(),
+        br.toDouble()
+    )
+
     operator fun div(b: Double): DriveVelocities {
         return DriveVelocities(
             this.frontLeft / b,
@@ -18,28 +24,45 @@ data class DriveVelocities(
             this.backRight / b
         )
     }
-    fun min() : Double {
+
+    fun min(): Double {
         return minOf(frontLeft, frontRight, backLeft, backRight)
     }
-    fun max() : Double {
+
+    fun max(): Double {
         return maxOf(frontLeft, frontRight, backLeft, backRight)
     }
-    fun asPercent() : DriveVelocities {
+
+    fun asPercent(): DriveVelocities {
         return this / max()
     }
 
-    internal fun applyDriftNormalizer(): Quadruple<Double>{
+    fun abs(): DriveVelocities {
+        return DriveVelocities(
+            abs(frontLeft),
+            abs(frontRight),
+            abs(backLeft),
+            abs(backRight)
+        )
+    }
+
+    fun average(): Double {
+        return (frontLeft + frontRight + backLeft + backRight) / 4
+    }
+
+    internal fun applyDriftNormalizer(): Quadruple<Double> {
         return applyDriftNormalizer(0.1)
     }
-    internal fun applyDriftNormalizer(tolerance: Double): Quadruple<Double>{
+
+    internal fun applyDriftNormalizer(tolerance: Double): Quadruple<Double> {
         val min = min()
         val max = max()
-        val dif = abs(max-min)
+        val dif = abs(max - min)
         if (dif >= tolerance) {
-            val frontLeft = 1+((dif * (((1 - frontLeft) / dif) - 1)))
-            val frontRight = 1+((dif * (((1 - frontRight) / dif) - 1)))
-            val backLeft = 1+((dif * (((1 - backLeft) / dif) - 1)))
-            val backRight = 1+((dif * (((1 - backRight) / dif) - 1)))
+            val frontLeft = 1 + ((dif * (((1 - frontLeft) / dif) - 1)))
+            val frontRight = 1 + ((dif * (((1 - frontRight) / dif) - 1)))
+            val backLeft = 1 + ((dif * (((1 - backLeft) / dif) - 1)))
+            val backRight = 1 + ((dif * (((1 - backRight) / dif) - 1)))
             return Quadruple(frontLeft, frontRight, backLeft, backRight)
         }
         return Quadruple(0.0)
