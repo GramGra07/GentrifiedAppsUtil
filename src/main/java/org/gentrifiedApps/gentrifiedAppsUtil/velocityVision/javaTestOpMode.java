189341af99ac14534056e56f5c126b6b913e6f93
Graden <101433010+GramGra07@users.noreply.github.com>
2025-09-06 20:21:50 -0600

    fixed

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/velocityVision/javaTestOpMode.java
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/velocityVision/javaTestOpMode.java
@@ -1,6 +1,5 @@
 package org.gentrifiedApps.gentrifiedAppsUtil.velocityVision;
 
-import org.gentrifiedApps.gentrifiedAppsUtil.decode.ColorPattern;
 import org.gentrifiedApps.gentrifiedAppsUtil.velocityVision.pipelines.bow.BlackAndWhiteDotDetector;
 import org.gentrifiedApps.gentrifiedAppsUtil.velocityVision.pipelines.bow.DotColor;
 import org.gentrifiedApps.gentrifiedAppsUtil.velocityVision.pipelines.bow.DotDetectionBuilder;
@@ -13,7 +12,6 @@ public class javaTestOpMode {
                     new Rect(50, 50, 100, 75), 10.0, 100.0
             )
     );
-    ColorPattern pattern = new ColorPattern(0, 2, 5);
 
     public static void main(String[] args) {
     }
