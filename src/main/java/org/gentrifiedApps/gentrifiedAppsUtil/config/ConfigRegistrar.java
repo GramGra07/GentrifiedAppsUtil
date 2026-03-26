60d31f8cb05907d0ee971d78f0f2bc315aebb117
Graden <101433010+GramGra07@users.noreply.github.com>
2026-03-25 20:59:36 -0600

    urgent fix

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/config/ConfigRegistrar.java
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/config/ConfigRegistrar.java
@@ -1,25 +1,25 @@
 package org.gentrifiedApps.gentrifiedAppsUtil.config;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
-import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
-
-
-public final class ConfigRegistrar {
-
-    static ConfigMaker config = new ConfigMaker("if you see this tell me")
-            .addMotor("f1", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 0)
-            .addMotor("fr", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 2)
-            .addMotor("bl", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 1)
-            .addMotor("br", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 3);
-
-    static boolean isEnabled = false;
-
-    private ConfigRegistrar() {
-    }
-
-    @OpModeRegistrar
-    public static void register(OpModeManager manager) {
-        if (!isEnabled) return;
-        manager.register(config.metaData(), new ConfigCreator(config));
-    }
-}
+//
+//import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
+//import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
+//
+//
+//public final class ConfigRegistrar {
+//
+//    static ConfigMaker config = new ConfigMaker("if you see this tell me")
+//            .addMotor("f1", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 0)
+//            .addMotor("fr", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 2)
+//            .addMotor("bl", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 1)
+//            .addMotor("br", ConfigMaker.ModuleType.CONTROL_HUB, ConfigMaker.MotorType.RevRoboticsUltraplanetaryHDHexMotor, 3);
+//
+//    static boolean isEnabled = false;
+//
+//    private ConfigRegistrar() {
+//    }
+//
+//    @OpModeRegistrar
+//    public static void register(OpModeManager manager) {
+//        if (!isEnabled) return;
+//        manager.register(config.metaData(), new ConfigCreator(config));
+//    }
+//}
