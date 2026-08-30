d86d47f6fe4782bdcbe31ce8fb3fd72418fea3e4
Graden <101433010+GramGra07@users.noreply.github.com>
2026-08-30 10:34:36 -0600

    added pid tuners for ease of testing pt 2

--- src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/pidtuners/Opmodes/PIDTuningOpModes.java
+++ src/main/java/org/gentrifiedApps/gentrifiedAppsUtil/pidtuners/Opmodes/PIDTuningOpModes.java
@@ -1,8 +1,9 @@
-package com.dacodingbeast.pidtuners.Opmodes;
+package org.gentrifiedApps.gentrifiedAppsUtil.pidtuners.Opmodes;
 
 
 import com.dacodingbeast.pidtuners.HardwareSetup.PivotSpecs;
 import com.dacodingbeast.pidtuners.HardwareSetup.SlideSpecs;
+import com.dacodingbeast.pidtuners.Opmodes.PIDCalculator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
@@ -14,12 +15,13 @@ class PIDTuningOpModes {
 
     public static Double TS = 0.5;
     public static Double OS = 0.5;
-    public static SlideSpecs s_specs = new SlideSpecs(6000,105,1,28,1.0,1.0,(1/20),400,0);
-    public static PivotSpecs p_specs = new PivotSpecs(6000,105,28,1.0,1.0,1.0,(1/20));
+    public static SlideSpecs s_specs = new SlideSpecs(6000, 105, 1, 28, 1.0, 1.0, (1 / 20), 400, 0);
+    public static PivotSpecs p_specs = new PivotSpecs(6000, 105, 28, 1.0, 1.0, 1.0, (1 / 20));
     public static Boolean ENABLED = false;
+
     private static OpModeMeta metaForClass(Class<? extends OpMode> cls) {
         return new OpModeMeta.Builder()
-                .setName(cls.getSimpleName() )
+                .setName(cls.getSimpleName())
                 .setGroup("PIDTuners")
                 .setFlavor(OpModeMeta.Flavor.TELEOP)
                 .build();
@@ -28,7 +30,7 @@ class PIDTuningOpModes {
     @OpModeRegistrar
     public static void register(OpModeManager manager) {
         if (!ENABLED) return;
-        manager.register(metaForClass(PIDCalculator.class),new PIDCalculator(p_specs,s_specs,OS,TS));
+        manager.register(metaForClass(PIDCalculator.class), new PIDCalculator(p_specs, s_specs, OS, TS));
     }
 }
 
